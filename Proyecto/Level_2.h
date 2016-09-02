6bf0af0c4163a503557d057e224fd8683e119479
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-09-02 18:16:24 -0500

    Nivel 2 Solidificado

--- Proyecto/Level_2.h
+++ Proyecto/Level_2.h
@@ -11,5 +11,12 @@
 @interface Level_2 : UIViewController
 @property (strong, nonatomic) IBOutlet UIButton *Back_To_Level;
 
+@property (strong, nonatomic) IBOutlet UILabel *Label_Lvl2;
+
+@property (strong, nonatomic) IBOutlet UIImageView *IMG_Lvl2;
+
+@property (strong, nonatomic) IBOutlet UIImageView *Animal_Pic;
+@property (strong, nonatomic) IBOutlet UIButton *Btt_L2_1;
+@property (strong, nonatomic) IBOutlet UIButton *Btt_L2_2;
 
 @end
--- Proyecto/Level_2.m
+++ Proyecto/Level_2.m
@@ -5,7 +5,6 @@
 //  Created by Martin Leon Preciado on 02/09/16.
 //  Copyright © 2016 UAG. All rights reserved.
 //
-
 #import "Level_2.h"
 
 @interface Level_2 ()
@@ -21,6 +20,8 @@ char Respuestas[8]={1,2,2,1,2,1,1,2};
 
 @implementation Level_2
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     Contador =0;
@@ -30,31 +31,83 @@ char Respuestas[8]={1,2,2,1,2,1,1,2};
     self.Animal_Name2  = [[NSMutableArray alloc] initWithObjects: @"Cat",@"Cat",@"Elephant",@"Horse",@"Toucan"@"Fish"@"Cow"@"Rhino",nil];
 }
 
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
+/*
+ 
+ #pragma mark - Navigation
+ 
+ 
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ 
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ 
+ // Get the new view controller using [segue destinationViewController].
+ 
+ // Pass the selected object to the new view controller.
+ 
+ }
+ 
+ */
+
 - (IBAction)Back_Level:(id)sender {
     [self performSegueWithIdentifier: @"Back_Level" sender:self];  //Se realiza la saga llamada a segunda ventada
 }
+
+
+
 - (IBAction)Btt1:(id)sender {
+    
     if(Respuestas[Contador]==1)
     {
-        
+        [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(Siguiente) userInfo:nil repeats:NO];
+        self.Label_Lvl2.text = @"Correct";
+        [self.IMG_Lvl2 setImage:[UIImage imageNamed:@"Correct.png"]];
+    }
+    else
+    {
+        [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(Siguiente) userInfo:nil repeats:NO];
+        self.Label_Lvl2.text = @"Incorrect";
+        [self.IMG_Lvl2 setImage:[UIImage imageNamed:@"Wrong"]];
     }
 }
 
+
+
 - (IBAction)Btt_2:(id)sender {
+    if(Respuestas[Contador]==2)
+    {
+        [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(Siguiente) userInfo:nil repeats:NO];
+        self.Label_Lvl2.text = @"Correct";
+        [self.IMG_Lvl2 setImage:[UIImage imageNamed:@"Correct.png"]];
+    }
+    else
+    {
+        [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(Siguiente) userInfo:nil repeats:NO];
+        self.Label_Lvl2.text = @"Incorrect";
+        [self.IMG_Lvl2 setImage:[UIImage imageNamed:@"Wrong"]];
+    }
 }
+
+-(void) Siguiente{
+    
+    Contador++;
+    self.Label_Lvl2.text = @"Chose an Option";
+    [self.IMG_Lvl2 setImage:[UIImage imageNamed:@"Question.png"]];
+    [self.Animal_Pic setImage:[UIImage imageNamed:self.Picture_name[Contador]]];
+    [self.Btt_L2_1 setTitle:self.Animal_Name[Contador] forState:UIControlStateNormal];
+    [self.Btt_L2_2 setTitle:self.Animal_Name2[Contador] forState:UIControlStateNormal];
+    
+    
+}
+
 
 @end
