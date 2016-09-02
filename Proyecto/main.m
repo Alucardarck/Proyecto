f5ee733ec8e6dd00650b661a51063c4982b9b7fb
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-09-01 19:26:45 -0500

    Initial Commit

---
+++ Proyecto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Proyecto
+//
+//  Created by Martin Leon Preciado on 01/09/16.
+//  Copyright © 2016 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
